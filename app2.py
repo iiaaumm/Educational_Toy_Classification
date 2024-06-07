07220a66362cdcc8e2a9cad82f1384ccd82dd382
iiaaumm <118011212+iiaaumm@users.noreply.github.com>
2024-06-08 02:51:27 +0700

    Update app2.py

--- app2.py
+++ app2.py
@@ -4,7 +4,7 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing import image
 from PIL import Image
 
-model_path = 'D:\STUDY\Year4\Development_Toy-A-10class\saved_model\'
+model_path = 'D:\STUDY\Year4\Development_Toy-A-10class\saved_model\Toy_classification_MobileNet.h5'
 
 # Function to load the model
 @st.cache(allow_output_mutation=True)
